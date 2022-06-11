7ca7fe66b1dd2f510ac4aac9c13e3d84ae896641
ManojRokana <106581578+ManojRokana@users.noreply.github.com>
2022-06-11 14:14:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,19 +24,19 @@ document.addEventListener("keydown", function (event){
 function makesound(key){
   switch (key) {
     case "w":
-      var crash = new Audio("sounds/crash.mp3");
+      var crash = new Audio("crash.mp3");
       crash.play();
       break;
     case "a":
-      var kick = new Audio("sounds/kick-bass.mp3");
+      var kick = new Audio("kick-bass.mp3");
       kick.play();
       break;
     case "s":
-      var snare = new Audio("sounds/snare.mp3");
+      var snare = new Audio("snare.mp3");
     snare.play();
       break;
     case "d":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      var tom1 = new Audio("tom-1.mp3");
       tom1.play();
       break;
     case "j":
@@ -48,7 +48,7 @@ function makesound(key){
       tom3.play();
       break;
     case "l":
-      var tom4 = new Audio("sounds/tom-4.mp3");
+      var tom4 = new Audio("tom-4.mp3");
       tom4.play();
       break;
     default:
